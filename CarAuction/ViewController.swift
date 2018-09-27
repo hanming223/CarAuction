039de4dbe3d67344495f66733e6797fcdc55724f
Han Ming <hanming223@gmail.com>
2018-09-28 03:40:11 +0800

    #1

--- CarAuction/ViewController.swift
+++ CarAuction/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import SwiftyDropbox
+
+let API_KEY = "dVa6Cjf4SrAAAAAAAAAAI1a9JG5jgPPbp7Fekw6Ungm01qwXTkoOhwcyiJlBuSbT"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        DropboxClientsManager.authorizedClient = DropboxClient.init(accessToken: API_KEY)
+        let client = DropboxClientsManager.authorizedClient
+        
+        for i in 0..<10{
+            client?.files.createFolderV2(path: "/" + String(100 - i)).response { response, error in
+                if let response = response {
+                    print(response)
+                } else if let error = error {
+                    print(error)
+                }
+            }
+        }
+        
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-CarAuctionTests/Pods-CarAuctionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarAuctionTests : NSObject
+@end
+@implementation PodsDummy_Pods_CarAuctionTests
+@end
